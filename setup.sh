0cd707447f38e55baaa4d78a6740dbb3b331ddfd
sultanfaqih <134555270+sultanfaqih@users.noreply.github.com>
2023-06-10 23:16:38 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,44 +22,6 @@ MYIP=$(wget -qO- ipinfo.io/ip);
 
 echo "Checking VPS"
 
-CEKEXPIRED () {
-
-today=$(date -d +1day +%Y-%m-%d)
-
-Exp1=$(curl -sS https://raw.githubusercontent.com/sultanfaqih/IP/main/ip | grep $MYIP | awk '{print $3}')
-
-if [[ $today < $Exp1 ]]; then
-
-echo -e "\e[32mSTATUS SCRIPT AKTIF...\e[0m"
-
-else
-
-echo -e "\e[31mSCRIPT ANDA EXPIRED!\e[0m";
-
-exit 0
-
-fi
-
-}
-
-IZIN=$(curl -sS https://raw.githubusercontent.com/sultanfaqih/IP/main/ip | awk '{print $4}' | grep $MYIP)
-
-if [ $MYIP = $IZIN ]; then
-
-echo -e "\e[32mPermission Accepted...\e[0m"
-
-CEKEXPIRED
-
-else
-
-echo -e "\e[31mPermission Denied!\e[0m";
-
-exit 0
-
-fi
-
-clear
-
 if [[ "$hst" != "$dart" ]]; then
 
 echo "$localip $(hostname)" >> /etc/hosts

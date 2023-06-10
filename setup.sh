8fd68e7d09f51efb249203803813f90a75d85650
sultanfaqih <134555270+sultanfaqih@users.noreply.github.com>
2023-06-10 23:35:02 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,37 +1,42 @@
 MYIP=$(curl -sS ipv4.icanhazip.com)
-
 red='\e[1;31m'
-
 green='\e[0;32m'
-
 yell='\e[1;33m'
-
 tyblue='\e[1;36m'
-
 NC='\e[0m'
 
-COLOR1='\033[0;35m'
-
-localip=$(hostname -I | cut -d\  -f1)
-
-hst=( `hostname` )
-
-dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
-
+# Getting
 MYIP=$(wget -qO- ipinfo.io/ip);
-
-echo "Checking VPS"
-
-if [[ "$hst" != "$dart" ]]; then
-
-echo "$localip $(hostname)" >> /etc/hosts
-
+echo "memeriksa vps anda"
+sleep 0.5
+CEKEXPIRED () {
+        today=$(date -d +1day +%Y -%m -%d)
+        Exp1=$(curl -sS https://raw.githubusercontent.com/mofukuy/Autoscript-by-azi/main/izin | grep $MYIP | awk '{print $3}')
+        if [[ $today < $Exp1 ]]; then
+        echo "status script aktif.."
+        else
+        echo "SCRIPT ANDA EXPIRED";
+        exit 0
+fi
+}
+IZIN=$(curl -sS https://raw.githubusercontent.com/mofukuy/Autoscript-by-azi/main/izin | awk '{print $4}' | grep $MYIP)
+if [ $MYIP = $IZIN ]; then
+echo "IZIN DI TERIMA!!"
+CEKEXPIRED
+else
+echo "Akses di tolak!! Benget sia hurung!!";
+exit 0
 fi
 
+
+localip=$(hostname -I | cut -d\  -f1)
+hst=( `hostname` )
+dart=$(cat /etc/hosts | grep -w `hostname` | awk '{print $2}')
+if [[ "$hst" != "$dart" ]]; then
+echo "$localip $(hostname)" >> /etc/hosts
+fi
 if [ -f "/root/log-install.txt" ]; then
-
 rm -fr /root/log-install.txt
-
 fi
 
 mkdir -p /etc/xray
